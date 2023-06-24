2262416bcbbf3efdab87a1a0ed61c28776e9348e
Ali Mohamed Ali <121377404+Ali-Mohamed3376@users.noreply.github.com>
2023-06-24 11:35:15 +0300

    Testing With Conflict

--- Final.Project.BL/Dtos/User/UserResetPasswordDto.cs
+++ Final.Project.BL/Dtos/User/UserResetPasswordDto.cs
@@ -3,5 +3,4 @@ public class UserResetPasswordDto
 {
     public string Email { get; set; } = string.Empty;
     public string NewPassword { get; set; } = string.Empty;
-    // test 2 by ali 222222
 }
