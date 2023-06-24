e1f9e6ed0ce08302992827f93c1a4265f0e81b8b
Ali Mohamed Ali <121377404+Ali-Mohamed3376@users.noreply.github.com>
2023-06-24 11:05:50 +0300

    Adding test two by ali

--- Final.Project.BL/Dtos/User/UserResetPasswordDto.cs
+++ Final.Project.BL/Dtos/User/UserResetPasswordDto.cs
@@ -3,4 +3,5 @@ public class UserResetPasswordDto
 {
     public string Email { get; set; } = string.Empty;
     public string NewPassword { get; set; } = string.Empty;
+    // test 2 by ali 222222
 }
