4dd12fcfb7a90616493dfbac42384ba13c7bc5e6
Ali Mohamed Ali <121377404+Ali-Mohamed3376@users.noreply.github.com>
2023-06-26 11:59:54 +0300

    Test Filteration

--- Final.Project.API/Program.cs
+++ Final.Project.API/Program.cs
@@ -87,6 +87,7 @@ builder.Services.AddScoped<IReviewRepo, ReviewRepo>();
 
 builder.Services.AddDbContext<ECommerceContext>(options => options
 
+
     .UseSqlServer(@"Server=DESKTOP-35F9698\SQLEXPRESS;Database=E-CommerceDB;Trusted_Connection=true;Encrypt=false"));
 //.UseSqlServer(@"Server=DESKTOP-85Q5KQD\SS17;Database=E-CommerceDBTest;Trusted_Connection=true;Encrypt=false"));
 //.UseSqlServer(@"Server=DESKTOP-85Q5KQD\SS17;Database=E-CommerceDBTest;Trusted_Connection=true;Encrypt=false"));
