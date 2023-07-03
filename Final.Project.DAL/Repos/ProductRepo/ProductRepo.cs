93654259cbe3c2a1f352503bfa94eaaca2a6d66b
Reham135 <reham7166@gmail.com>
2023-07-03 11:22:48 +0300

    filtering by brice after discount

--- Final.Project.DAL/Repos/ProductRepo/ProductRepo.cs
+++ Final.Project.DAL/Repos/ProductRepo/ProductRepo.cs
@@ -81,11 +81,11 @@ public class ProductRepo : GenericRepo<Product>, IProductRepo
 
         if (parametars.MaxPrice > 0)
         {
-            products = products.Where(q => q.Price <= parametars.MaxPrice);
+            products = products.Where(q => q.Price - (q.Price * q.Discount / 100) <= parametars.MaxPrice);
         }
         if (parametars.MinPrice > 0)
         {
-            products = products.Where(q => q.Price >= parametars.MinPrice);
+            products = products.Where(q => q.Price-(q.Price * q.Discount/100) >= parametars.MinPrice);
         }
 
         if (parametars.Rating > 0)
